f15498454c9192d39a66feceafe5f6a681afc6c8
Christopher Cote <chris@entropealabs.com>
2016-06-01 12:13:54 -0500

    allow auth token to be pased in on process creation

--- lib/placemeter.ex
+++ lib/placemeter.ex
@@ -1,8 +1,8 @@
 defmodule Placemeter do
     use GenServer
 
-    def start_link do
-        GenServer.start_link(__MODULE__, [], name: __MODULE__)
+    def start_link(token) do
+        GenServer.start_link(__MODULE__, token, name: __MODULE__)
     end
 
     def measurementpoints(time_ago \\ 1000) do
@@ -13,29 +13,33 @@ defmodule Placemeter do
         GenServer.call(__MODULE__, {:measurementpoint, id, time_ago}, 10000)
     end
 
+    def init(token) do
+        {:ok, %{:token => token}}
+    end
+
     def handle_call({:measurementpoints, time_ago}, _from, state) do
-        case Placemeter.Client.measurementpoints do
+        case Placemeter.Client.measurementpoints(state.token) do
             {:ok, response} ->
-                {:reply, {:ok, Enum.map(response, &get_point(&1, time_ago))}, state}
+                {:reply, {:ok, Enum.map(response, &get_point(state.token, &1, time_ago))}, state}
             {:error, reason} ->
                 {:reply, {:error, reason}, state}
         end
     end
 
     def handle_call({:measurementpoint, id, time_ago}, _from, state) do
-        case Placemeter.Client.measurementpoints(id) do
+        case Placemeter.Client.measurementpoints(state.token, id) do
             {:ok, response} ->
-                {:reply, {:ok, get_point(response, time_ago)}, state}
+                {:reply, {:ok, get_point(state.token, response, time_ago)}, state}
             {:error, reason} ->
                 {:reply, {:error, reason}, state}
         end
     end
 
-    def get_point(point, time_ago) do
+    def get_point(token, point, time_ago) do
         IO.inspect point
         now = :erlang.system_time(:seconds)
         yesterday = now - time_ago
-        case Placemeter.Client.measurementpoints(point.id, yesterday, now) do
+        case Placemeter.Client.measurementpoints(token, point.id, yesterday, now) do
             {:ok, %{"data" => data}} ->
                 Enum.map(data, fn(record) -> %{:id => point.id, :data => record} end)
             {:error, reason} ->
--- lib/placemeter/client.ex
+++ lib/placemeter/client.ex
@@ -1,7 +1,6 @@
 defmodule Placemeter.Client do
     use HTTPoison.Base
 
-    @token Application.get_env(:placemeter, :auth_token)
     @endpoint "https://api.placemeter.net/api/v1/"
 
     defmodule Point do
@@ -20,13 +19,8 @@ defmodule Placemeter.Client do
         @endpoint <> url
     end
 
-    def process_request_headers(headers) do
-        token = Application.get_env(:placemeter, :auth_token)
-        headers |> Dict.put(:"Authorization", "Token #{token}")
-    end
-
-    def measurementpoints do
-        case Placemeter.Client.get("measurementpoints") do
+    def measurementpoints(token) do
+        case Placemeter.Client.get("measurementpoints", Dict.put([], :"Authorization", "Token #{token}")) do
             {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
                 {:ok, body
                     |> Poison.decode!(as: [%Point{location: %Location{}, metrics: [%Metric{}], classes: []}])
@@ -36,8 +30,8 @@ defmodule Placemeter.Client do
         end
     end
 
-    def measurementpoints(point_id) do
-        case Placemeter.Client.get("measurementpoints/#{point_id}") do
+    def measurementpoints(token, point_id) do
+        case Placemeter.Client.get("measurementpoints/#{point_id}", Dict.put([], :"Authorization", "Token #{token}")) do
             {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
                 {:ok, body
                     |> Poison.decode!(as: %Point{location: %Location{}, metrics: [%Metric{}], classes: []})
@@ -47,8 +41,8 @@ defmodule Placemeter.Client do
         end
     end
 
-    def measurementpoints(point_id, start, en, res \\ "minute", metrics \\ "all", classes \\ "all") do
-        case Placemeter.Client.get("measurementpoints/#{point_id}/data", [], params: %{start: start, "end": en, resolution: res, classes: classes}) do
+    def measurementpoints(token, point_id, start, en, res \\ "minute", metrics \\ "all", classes \\ "all") do
+        case Placemeter.Client.get("measurementpoints/#{point_id}/data", Dict.put([], :"Authorization", "Token #{token}"), params: %{start: start, "end": en, resolution: res, classes: classes}) do
             {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
                 {:ok, body
                     |> Poison.Parser.parse!
